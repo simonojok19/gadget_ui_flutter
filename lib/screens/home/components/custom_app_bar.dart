8810cd9985885691b2ee8522ab3d45636ae1aaf7
simonojok19 <simonojok19@gmail.com>
2020-11-08 17:32:41 +0300

    adding stylings

--- lib/screens/home/components/custom_app_bar.dart
+++ lib/screens/home/components/custom_app_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gadgets_store_app/constants/constants.dart';
 
 class CustomAppBar extends StatefulWidget {
   @override
@@ -8,8 +9,44 @@ class CustomAppBar extends StatefulWidget {
 class _CustomAppBarState extends State<CustomAppBar> {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return SafeArea(
-        child: Container()
+        child: Container(
+          height: size.height * 0.1,
+          padding: EdgeInsets.all(appPadding),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Icon(
+                Icons.short_text_rounded,
+                color: white,
+                size: 25,
+              ),
+              RichText(
+                text: TextSpan(
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold
+                  ),
+                  children: [
+                    TextSpan(
+                      text: 'Erupt',
+                      style: TextStyle(
+                        color: yellow
+                      )
+                    ),
+                    TextSpan(
+                        text: '.',
+                        style: TextStyle(
+                            color: red
+                        )
+                    )
+                  ]
+                ),
+              )
+            ],
+          ),
+        )
     );
   }
 }
