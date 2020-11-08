76e6845c04e692ad9be431b13cc42582da9d8601
simonojok19 <simonojok19@gmail.com>
2020-11-08 19:29:15 +0300

    added star rating widget

---
+++ lib/screens/home/widgets/star_rating.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class StarRating extends StatefulWidget {
+  @override
+  _StarRatingState createState() => _StarRatingState();
+}
+
+class _StarRatingState extends State<StarRating> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
