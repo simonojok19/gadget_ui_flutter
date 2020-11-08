28d2d18015713fce1b40a17191e7bed2cc924ff8
simonojok19 <simonojok19@gmail.com>
2020-11-08 19:32:50 +0300

    adding rating

--- lib/screens/home/widgets/star_rating.dart
+++ lib/screens/home/widgets/star_rating.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
 class StarRating extends StatefulWidget {
+  final double rating;
+
+  const StarRating({Key key, this.rating}) : super(key: key);
   @override
   _StarRatingState createState() => _StarRatingState();
 }
@@ -8,6 +11,13 @@ class StarRating extends StatefulWidget {
 class _StarRatingState extends State<StarRating> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: List.generate(5, (index) {
+        if (index < (widget.rating).round()) {
+          return
+        }
+      }),
+    );
   }
 }
