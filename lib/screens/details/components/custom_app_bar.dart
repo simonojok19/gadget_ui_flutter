eeab7fc9ff8c3cf82e90bfb978abde849a7a5774
simonojok19 <simonojok19@gmail.com>
2020-11-08 22:01:20 +0300

    done wiring up the navigator

--- lib/screens/details/components/custom_app_bar.dart
+++ lib/screens/details/components/custom_app_bar.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:gadgets_store_app/constants/constants.dart';
 
 class CustomAppBar extends StatefulWidget {
   @override
@@ -8,6 +10,49 @@ class CustomAppBar extends StatefulWidget {
 class _CustomAppBarState extends State<CustomAppBar> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    Size size = MediaQuery.of(context).size;
+    return SafeArea(
+      child: Container(
+        height: size.height * 0.1,
+        padding: EdgeInsets.all(appPadding),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            InkWell(
+              onTap: () {
+                Navigator.pop(context);
+              },
+                child: Icon(Icons.arrow_back,
+                  color: black,
+                  size: 25,
+                )
+            ),
+            RichText(
+              text: TextSpan(
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold
+                ),
+                children: [
+                  TextSpan(
+                    text: 'Erupt',
+                    style: TextStyle(
+                      color: yellow
+                    )
+                  ),
+                  TextSpan(
+                    text: '.',
+                    style: TextStyle(
+                      color: red
+                    )
+                  )
+                ]
+              ),
+            ),
+            Icon(Icons.search_rounded, color: black, size: 25,)
+          ],
+        ),
+      ),
+    );
   }
 }
