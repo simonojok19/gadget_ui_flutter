28a29010e77a50c828a0b6b63507f99b62cc7cf1
simonojok19 <simonojok19@gmail.com>
2020-11-08 18:47:35 +0300

    added some methods

--- lib/screens/home/components/popular_products.dart
+++ lib/screens/home/components/popular_products.dart
@@ -27,71 +27,103 @@ class PopularProducts extends StatelessWidget {
       width: size.width * 0.3,
       child: Stack(
         children: [
-          Padding(
+          _buildDiscount(size, popularProductModel),
+          _buildPriceCard(size, popularProductModel),
+          _buildImageCard(size, popularProductModel),
+
+        ],
+      ),
+    );
+  }
+
+  Positioned _buildImageCard(Size size, PopularProductModel popularProductModel) {
+    return Positioned(
+          bottom: size.height * 0.04,
+          right: size.height * 0.001,
+          child: Padding(
             padding: const EdgeInsets.symmetric(vertical: appPadding / 4),
             child: Container(
               width: size.width * 0.2,
               height: size.height * 0.09,
               decoration: BoxDecoration(
-                color: brown,
-                borderRadius: BorderRadius.circular(20.0)
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: appPadding / 4),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      '\$${popularProductModel.price}',
-                      style: TextStyle(
-                        color: white,
-                        fontSize: 20
-                      ),
-                    ),
-                    Text(
-                      '\$${popularProductModel.oldPrice}',
-                      style: TextStyle(
-                          color: yellow,
-                          fontSize: 12,
-                        decoration: TextDecoration.lineThrough
-                      ),
+                  color: white,
+                  borderRadius: BorderRadius.circular(20.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: black.withOpacity(0.4),
+                      offset: Offset(5,5),
+                      blurRadius: 5
                     )
-                  ],
-                ),
+                ]
+              ),
+              child: Image(
+                fit: BoxFit.contain,
+                image: AssetImage(popularProductModel.imageUrl),
               ),
             ),
           ),
+        );
+  }
 
-          Positioned(
-            bottom: size.height * 0.04,
-            right: size.height * 0.001,
+  Padding _buildPriceCard(Size size, PopularProductModel popularProductModel) {
+    return Padding(
+          padding: const EdgeInsets.symmetric(vertical: appPadding / 4),
+          child: Container(
+            width: size.width * 0.2,
+            height: size.height * 0.09,
+            decoration: BoxDecoration(
+              color: brown,
+              borderRadius: BorderRadius.circular(20.0)
+            ),
             child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: appPadding / 4),
-              child: Container(
-                width: size.width * 0.2,
-                height: size.height * 0.09,
-                decoration: BoxDecoration(
-                    color: white,
-                    borderRadius: BorderRadius.circular(20.0),
-                    boxShadow: [
-                      BoxShadow(
-                        color: black.withOpacity(0.4),
-                        offset: Offset(5,5),
-                        blurRadius: 5
-                      )
-                  ]
-                ),
-                child: Image(
-                  fit: BoxFit.contain,
-                  image: AssetImage(popularProductModel.imageUrl),
-                ),
+              padding: EdgeInsets.symmetric(horizontal: appPadding / 4),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    '\$${popularProductModel.price}',
+                    style: TextStyle(
+                      color: white,
+                      fontSize: 20
+                    ),
+                  ),
+                  Text(
+                    '\$${popularProductModel.oldPrice}',
+                    style: TextStyle(
+                        color: yellow,
+                        fontSize: 12,
+                      decoration: TextDecoration.lineThrough
+                    ),
+                  )
+                ],
               ),
             ),
           ),
+        );
+  }
 
-        ],
-      ),
-    );
+  Positioned _buildDiscount(Size size, PopularProductModel popularProductModel) {
+    return Positioned(
+          right: size.width * 0.04,
+          bottom: size.height * 0.11,
+          child: Container(
+            width: size.width * 0.05,
+            height: size.height * 0.05,
+            decoration: BoxDecoration(
+              color: red,
+              borderRadius: BorderRadius.circular(5)
+            ),
+            child: Text(
+              '${popularProductModel.discount}%',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                color: white,
+                fontSize: 8,
+                height: 2,
+              ),
+            ),
+          ),
+        );
   }
 }
